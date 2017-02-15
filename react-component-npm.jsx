4a6789376c1c14482a67f975707cec6853166c1e
Jun Li <jun@quandl.com>
2017-02-15 08:40:44 -0500

    greeting message

---
+++ react-component-npm.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './react-component-npm.scss';
+
+export default React.createClass({
+  render: function() {
+    return <div className="react-component-npm">Hi from react-friebaseui</div>;
+  }
+});
